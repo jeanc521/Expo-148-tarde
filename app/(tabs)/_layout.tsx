cb6cd3a605ae4d97f94ea3d8460d9d5374eed1a5
jeanc521 <jeanromoaldoc@gmail.com>
2025-05-12 16:43:45 -0300

    Add files via upload

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,18 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Text } from 'react-native';
+
+
+
+
+export default function TabLayout() {
+ 
+
+  return (
+    <Tabs>
+      <Tabs.Screen name='index' options={{headerShown: false, tabBarIcon: () => (<Text>🐱‍🏍</Text>)}}></Tabs.Screen>
+      <Tabs.Screen name='explore' options={{headerShown: false, tabBarIcon: () => (<Text>🐱‍👤</Text>)}}></Tabs.Screen>
+      <Tabs.Screen name='list' options={{headerShown: false, tabBarIcon: () => (<Text>🐱‍👓</Text>)}}></Tabs.Screen>
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/list.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+
+export default function TabThreeScreen() {
+    return(
+        <>
+        <Text>Terceira pagina</Text>
+        </>
+    )
+}
+
+const style = StyleSheet.create({
+
+})
+
